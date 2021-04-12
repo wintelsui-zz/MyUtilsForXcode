f8c2170b7f06b34e05e7e67760c9c6fdfb9d905d
wintelsui <wintelsui@hotmail.com>
2021-04-12 09:30:19 +0800

    Bing Start
    Bing Start

---
+++ MyUtils/MyUtils/TranslationEngine/BingTranslationEngine.h
@@ -0,0 +1,17 @@
+//
+//  BingTranslationEngine.h
+//  MyUtils
+//
+//  Created by smalltalk on 12/4/2021.
+//  Copyright © 2021 wintelsui. All rights reserved.
+//
+
+#import "XTranslationEngine.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BingTranslationEngine : XTranslationEngine
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyUtils/MyUtils/TranslationEngine/BingTranslationEngine.m
@@ -0,0 +1,13 @@
+//
+//  BingTranslationEngine.m
+//  MyUtils
+//
+//  Created by smalltalk on 12/4/2021.
+//  Copyright © 2021 wintelsui. All rights reserved.
+//
+
+#import "BingTranslationEngine.h"
+
+@implementation BingTranslationEngine
+
+@end
