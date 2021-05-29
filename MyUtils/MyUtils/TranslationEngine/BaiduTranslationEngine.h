8ac227453efd6583e1613bb89ad4423ce70e1fba
wintelsui <wintelsui@hotmail.com>
2021-05-29 22:58:26 +0800

    尚未实现的引擎
    尚未实现的引擎

---
+++ MyUtils/MyUtils/TranslationEngine/BaiduTranslationEngine.h
@@ -0,0 +1,17 @@
+//
+//  BaiduTranslationEngine.h
+//  MyUtils
+//
+//  Created by smalltalk on 29/5/2021.
+//  Copyright © 2021 wintelsui. All rights reserved.
+//
+
+#import "XTranslationEngine.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BaiduTranslationEngine : XTranslationEngine
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyUtils/MyUtils/TranslationEngine/BaiduTranslationEngine.m
@@ -0,0 +1,13 @@
+//
+//  BaiduTranslationEngine.m
+//  MyUtils
+//
+//  Created by smalltalk on 29/5/2021.
+//  Copyright © 2021 wintelsui. All rights reserved.
+//
+
+#import "BaiduTranslationEngine.h"
+
+@implementation BaiduTranslationEngine
+
+@end
